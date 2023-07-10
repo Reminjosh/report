6fa7abf34925e92df8be42ae6f176fdbfe3785a8
Remin jose John kennady <jreminjose@msservicesinc.com>
2023-07-10 18:07:09 +0530

    components added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {SubsidyComponent} from "./subsidy/subsidy.component";
+import {BudgetComponent} from "./budget/budget.component";
+import {HeaderComponent} from "./header/header.component";
+import {FooterComponent} from "./footer/footer.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'subsidy',
+    component: SubsidyComponent
+  },
+  {
+    path: 'budget',
+    component: BudgetComponent
+  },
+  {
+    path: 'header',
+    component: HeaderComponent
+  },
+  {
+    path: 'footer',
+    component: FooterComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BudgetComponent } from './budget/budget.component';
+import { SubsidyComponent } from './subsidy/subsidy.component';
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BudgetComponent,
+    SubsidyComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
